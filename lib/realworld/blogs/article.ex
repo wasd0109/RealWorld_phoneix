0932290c05c0f73e7aad153658ae02f810cad25e
Ken Cheung <wasd0109.dev@gmail.com>
2022-09-04 16:24:53 +0900

    Ch3 completed

--- lib/realworld/blogs/article.ex
+++ lib/realworld/blogs/article.ex
@@ -2,17 +2,26 @@ defmodule Realworld.Blogs.Article do
   use Ecto.Schema
   import Ecto.Changeset
 
+  alias Realworld.Blogs.Comment
+  alias Realworld.Blogs.Tag
+  alias Realworld.Blogs.ArticleTag
+
   schema "articles" do
     field :body, :string
     field :title, :string
-
+    has_many :comments, Comment, on_delete: :delete_all
+    many_to_many :tags, Tag,
+      join_through: ArticleTag,
+      on_replace: :delete,
+      on_delete: :delete_all
     timestamps()
   end
 
   @doc false
-  def changeset(article, attrs) do
+  def changeset(article, attrs, tags \\[]) do
     article
     |> cast(attrs, [:title, :body])
     |> validate_required([:title, :body])
+    |> put_assoc(:tags, tags)
   end
 end
--- test/support/fixtures/blogs_fixtures.ex
+++ test/support/fixtures/blogs_fixtures.ex
@@ -18,4 +18,38 @@ defmodule Realworld.BlogsFixtures do
 
     article
   end
+
+  @doc """
+  Generate a comment.
+  """
+  def comment_fixture(attrs \\ %{}) do
+    {:ok, comment} =
+      attrs
+      |> Enum.into(%{
+        body: "some body",
+        article_id: Map.get(article_fixture(),:id)
+      })
+      |> Realworld.Blogs.create_comment()
+
+    comment
+  end
+
+  @doc """
+  Generate a unique tag tag.
+  """
+  def unique_tag_tag, do: "some tag#{System.unique_integer([:positive])}"
+
+  @doc """
+  Generate a tag.
+  """
+  def tag_fixture(attrs \\ %{}) do
+    {:ok, tag} =
+      attrs
+      |> Enum.into(%{
+        tag: unique_tag_tag()
+      })
+      |> Realworld.Blogs.create_tag()
+
+    tag
+  end
 end
